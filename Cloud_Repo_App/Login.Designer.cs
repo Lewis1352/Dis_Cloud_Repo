f05578746ab406b448f3e8a1fc52878f7a154b26
Lewis Watson <lewiswat@ntlworld.com>
2020-02-19 22:46:42 +0000

    added basic login in design

--- Cloud_Repo_App/Login.Designer.cs
+++ Cloud_Repo_App/Login.Designer.cs
@@ -28,20 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username_textbox = new System.Windows.Forms.TextBox();
+            this.password_textbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.login_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // username_textbox
+            // 
+            this.username_textbox.Location = new System.Drawing.Point(307, 169);
+            this.username_textbox.Name = "username_textbox";
+            this.username_textbox.Size = new System.Drawing.Size(180, 22);
+            this.username_textbox.TabIndex = 2;
+            // 
+            // password_textbox
+            // 
+            this.password_textbox.Location = new System.Drawing.Point(307, 197);
+            this.password_textbox.Name = "password_textbox";
+            this.password_textbox.Size = new System.Drawing.Size(180, 22);
+            this.password_textbox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(127, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(508, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Welcome to the cloud storage application";
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(410, 241);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(77, 29);
+            this.login_button.TabIndex = 5;
+            this.login_button.Text = "Login";
+            this.login_button.UseVisualStyleBackColor = true;
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(885, 565);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.password_textbox);
+            this.Controls.Add(this.username_textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Cloud Repo";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username_textbox;
+        private System.Windows.Forms.TextBox password_textbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button login_button;
     }
 }
 
